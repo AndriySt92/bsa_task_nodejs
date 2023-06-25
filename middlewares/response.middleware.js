0303fd388a9031c39dee239d8f78c76987ff9737
AndriySt92 <andriyua-1992@mail.ru>
2023-06-25 19:49:01 +0300

    Fixed response middliware

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,6 +1,6 @@
-const responseMiddleware = (req, res, next) => {
+const responseMiddleware = (_, res, next) => {
   if (res.err) {
-    return res.json({ error: true, message: `${res.err}` })
+    return res.status(res.errCode).json({ error: true, message: `${res.err}` })
   } else if (res.data) {
     return res.json(res.data)
   }
