53d96925301984cc825462b26d86c3d4097e54f5
AndriySt92 <andriyua-1992@mail.ru>
2023-06-25 17:40:33 +0300

    Added auth routers

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,22 +1,22 @@
-import { Router } from "express";
-import { authService } from "../services/authService.js";
-import { responseMiddleware } from "../middlewares/response.middleware.js";
+import { Router } from 'express'
+import { authService } from '../services/authService.js'
+import { responseMiddleware } from '../middlewares/response.middleware.js'
 
-const router = Router();
+const router = Router()
 
-router.post(
-  "/login",
-  (req, res, next) => {
-    try {
-      // TODO: Implement login action (get the user if it exist with entered credentials)
-      res.data = data;
-    } catch (err) {
-      res.err = err;
-    } finally {
-      next();
-    }
-  },
-  responseMiddleware
-);
+router.post('/login', (req, res, next) => {
+  try {
+    const { email, password } = req.body
+    const user = authService.login({ email, password })
+    res.send(user)
+  } catch (err) {
+    res.status(404).send({
+      error: true,
+      message: 'User no found',
+    })
+  } finally {
+    next()
+  }
+})
 
-export { router };
+export { router }
