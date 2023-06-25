62ef51f55ac683c20b4f498e7a38d8cbef03fd9f
AndriySt92 <andriyua-1992@mail.ru>
2023-06-25 22:21:53 +0300

    Added fight routes

--- routes/fightRoutes.js
+++ routes/fightRoutes.js
@@ -1,13 +1,127 @@
 import { Router } from "express";
-import { fightersService } from "../services/fightService.js";
+import { fightService } from "../services/fightService.js";
 import {
-  createUserValid,
-  updateUserValid,
-} from "../middlewares/user.validation.middleware.js";
+  createFightValid,
+  updateFightValid,
+} from "../middlewares/fight.validation.middleware.js";
 import { responseMiddleware } from "../middlewares/response.middleware.js";
 
 const router = Router();
 
-// OPTIONAL TODO: Implement route controller for fights
+//get all fighters
+router.get(
+  '/',
+  (_, res, next) => {
+    try {
+      const fights = fightService.getAll()
+
+      if (!fights.length) {
+        res.err = 'Fights not found'
+        res.errCode = 404
+      } else {
+        res.data = fights
+      }
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
+    }
+  },
+  responseMiddleware,
+)
+
+//create fight
+router.post(
+  '/',
+  createFightValid,
+  (req, res, next) => {
+    try {
+      const fightData = req.body
+      const fight = fightService.create(fightData)
+      res.data = fight
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
+    }
+  },
+  responseMiddleware,
+)
+
+// find fight by id
+router.get(
+  '/:id',
+  (req, res, next) => {
+    try {
+      const { id } = req.params
+      const fight = fightService.getById(id)
+
+      if (!fight) {
+        res.err = 'Fight not found'
+        res.errCode = 404
+      } else {
+        res.data = fight
+      }
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
+    }
+  },
+  responseMiddleware,
+)
+
+//update fight
+router.put(
+  '/:id',
+  updateFightValid,
+  (req, res, next) => {
+    try {
+      const { id } = req.params
+      const fightInfo = req.body
+      const updatedFight = fightService.update(id, fightInfo)
+   
+      if (!updatedFight.id) {
+        res.err = 'Fight not found'
+        res.errCode = 404
+      } else {
+        res.data = updatedFight
+      }
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
+    }
+  },
+  responseMiddleware,
+)
+
+//delete fight
+router.delete(
+  '/:id',
+  (req, res, next) => {
+    try {
+      const { id } = req.params
+      const deletedFight = fightService.delete(id)
+
+      if (!deletedFight.length) {
+        res.err = 'Fight not found'
+        res.errCode = 404
+      } else {
+        res.data = deletedFight
+      }
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
+    }
+  },
+  responseMiddleware,
+)
 
 export { router };
