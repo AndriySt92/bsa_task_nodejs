10be6c749fc948b6ca58b7239e9878c69648f38b
AndriySt92 <andriyua-1992@mail.ru>
2023-06-25 22:21:01 +0300

    Added fightService methods

--- services/fightService.js
+++ services/fightService.js
@@ -1,9 +1,34 @@
 import { fightRepository } from "../repositories/fightRepository.js";
 
-class FightersService {
-  // OPTIONAL TODO: Implement methods to work with fights
+class FightService {
+  create(fighter) {
+    return fightRepository.create(fighter)
+  }
+  getAll() {
+    return fightRepository.getAll()
+  }
+
+  getById(id) {
+    return fightRepository.getOne({ id })
+  }
+
+  update(id, data) {
+    return fightRepository.update(id, data)
+  }
+
+  delete(id) {
+    return fightRepository.delete(id)
+  }
+  
+  search(search) {
+    const item = fightRepository.getOne(search)
+    if (!item) {
+      return null
+    }
+    return item
+  }
 }
 
-const fightersService = new FightersService();
+const fightService = new FightService();
 
-export { fightersService };
+export { fightService };
