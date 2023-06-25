3d86466fe2c46088df8acb37874b9d83bf25b572
AndriySt92 <andriyua-1992@mail.ru>
2023-06-25 19:50:23 +0300

    Implemented response middleware to user routes. Fixed user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,105 +1,124 @@
 import { Router } from 'express'
 import { userService } from '../services/userService.js'
 import { createUserValid, updateUserValid } from '../middlewares/user.validation.middleware.js'
+import { responseMiddleware } from '../middlewares/response.middleware.js'
 
 const router = Router()
 
 //get all users
-router.get('/', (_, res) => {
-  try {
-    const users = userService.getAllUsers()
+router.get(
+  '/',
+  (_, res, next) => {
+    try {
+      const users = userService.getAllUsers()
 
-    if (!users) {
-      res.status(400).send({
-        error: true,
-        message: 'There are no users in the database',
-      })
+      if (!users.length) {
+        res.err = 'Users no found'
+        res.errCode = 404
+      } else {
+        res.data = users
+      }
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
     }
-
-    res.send(users)
-  } catch (e) {
-    res.status(500).send({
-      error: true,
-      message: 'Internal server error',
-    })
-  }
-})
+  },
+  responseMiddleware,
+)
 
 //create user
-router.post('/', createUserValid, (req, res) => {
-  const user = req.body
-  try {
-    res.send(userService.createUser(user))
-  } catch (e) {
-    res.status(500).send({
-      error: true,
-      message: 'Internal server error',
-    })
-  }
-})
+router.post(
+  '/',
+  createUserValid,
+  (req, res, next) => {
+    try {
+      const userData = req.body
+      const user = userService.createUser(userData)
+      res.data = user
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
+    }
+  },
+  responseMiddleware,
+)
 
 // find by id user
-router.get('/:id', (req, res) => {
-  const { id } = req.params
-  try {
-    const user = userService.getUserById(id)
+router.get(
+  '/:id',
+  (req, res, next) => {
+    try {
+      const { id } = req.params
+      const user = userService.getUserById(id)
 
-    if (!user) {
-      res.status(404).send({
-        error: true,
-        message: 'User not found',
-      })
+      if (!user) {
+        res.err = 'User no found'
+        res.errCode = 404
+      } else {
+        res.data = user
+      }
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
     }
-    res.send(user)
-  } catch (e) {
-    res.status(500).send({
-      error: true,
-      message: 'Internal server error',
-    })
-  }
-})
+  },
+  responseMiddleware,
+)
 
 //update user
-router.put('/:id', updateUserValid, (req, res) => {
-  try {
-    const { id } = req.params
-    const userInfo = req.body
-    const updatedUser = userService.updateUser(id, userInfo)
-
-    if (!updatedUser) {
-      res.status(404).send({
-        error: true,
-        message: 'User no found',
-      })
+router.put(
+  '/:id',
+  updateUserValid,
+  (req, res, next) => {
+    try {
+      const { id } = req.params
+      const userInfo = req.body
+      const updatedUser = userService.updateUser(id, userInfo)
+      console.log(updatedUser)
+      if (!updatedUser.id) {
+        res.err = 'User no found'
+        res.errCode = 404
+      } else {
+        res.data = updatedUser
+      }
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
     }
-    res.send(updatedUser)
-  } catch (e) {
-    res.status(500).send({
-      error: true,
-      message: 'Internal server error',
-    })
-  }
-})
+  },
+  responseMiddleware,
+)
 
 //delete user
-router.delete('/:id', (req, res) => {
-  try {
-    const { id } = req.params
-    const deletedUser = userService.deleteUser(id)
+router.delete(
+  '/:id',
+  (req, res, next) => {
+    try {
+      const { id } = req.params
+      const deletedUser = userSice.deleteUser(id)
 
-    if (!deletedUser) {
-      res.status(404).send({
-        error: true,
-        message: 'User no found',
-      })
+      if (!deletedUser.length) {
+        res.err = 'User no found'
+        res.errCode = 404
+      } else {
+        res.data = deletedUser
+      }
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
     }
-    res.send(deletedUser)
-  } catch (e) {
-    res.status(500).send({
-      error: true,
-      message: 'Internal server error',
-    })
-  }
-})
+  },
+  responseMiddleware,
+)
 
 export { router }
