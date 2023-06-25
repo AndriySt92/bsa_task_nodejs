7cd8ef77f7a6469753a28de7433078c3ee0b29f7
AndriySt92 <andriyua-1992@mail.ru>
2023-06-25 16:35:55 +0300

    Added userService methods

--- services/userService.js
+++ services/userService.js
@@ -1,17 +1,39 @@
-import { userRepository } from "../repositories/userRepository.js";
+import { userRepository } from '../repositories/userRepository.js'
 
 class UserService {
-  // TODO: Implement methods to work with user
+  createUser(user) {
+    return userRepository.create(user)
+  }
+
+  getAllUsers() {
+    return userRepository.getAll()
+  }
+
+  getUserById(id) {
+    return userRepository.getOne({ id })
+  }
+
+  addUser(data) {
+    return userRepository.create(data)
+  }
+
+  updateUser(id, data) {
+    return userRepository.update(id, data)
+  }
+
+  deleteUser(id) {
+    return userRepository.delete(id)
+  }
 
   search(search) {
-    const item = userRepository.getOne(search);
+    const item = userRepository.getOne(search)
     if (!item) {
-      return null;
+      return null
     }
-    return item;
+    return item
   }
 }
 
-const userService = new UserService();
+const userService = new UserService()
 
-export { userService };
+export { userService }
