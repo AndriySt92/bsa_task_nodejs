fd68d952259243980e4b8fbe9890d7008e008f39
AndriySt92 <andriyua-1992@mail.ru>
2023-06-25 21:32:32 +0300

    Added fighter routes

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -8,6 +8,125 @@ import {
 
 const router = Router();
 
-// TODO: Implement route controllers for fighter
+//get all fighters
+router.get(
+  '/',
+  (_, res, next) => {
+    try {
+      const fighters = fighterService.getAll()
+
+      if (!fighters.length) {
+        res.err = 'Fighters not found'
+        res.errCode = 404
+      } else {
+        res.data = fighters
+      }
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
+    }
+  },
+  responseMiddleware,
+)
+
+//create fighter
+router.post(
+  '/',
+  createFighterValid,
+  (req, res, next) => {
+    try {
+      const fighterData = req.body
+
+      if(!fighterData.health){
+        fighterData.health = 100
+      }
+
+      const fighter = fighterService.create(fighterData)
+      res.data = fighter
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
+    }
+  },
+  responseMiddleware,
+)
+
+// find fighter by id
+router.get(
+  '/:id',
+  (req, res, next) => {
+    try {
+      const { id } = req.params
+      const fighter = fighterService.getById(id)
+
+      if (!fighter) {
+        res.err = 'Fighter not found'
+        res.errCode = 404
+      } else {
+        res.data = fighter
+      }
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
+    }
+  },
+  responseMiddleware,
+)
+
+//update fighter
+router.put(
+  '/:id',
+  updateFighterValid,
+  (req, res, next) => {
+    try {
+      const { id } = req.params
+      const fighterInfo = req.body
+      const updatedFighter = fighterService.update(id, fighterInfo)
+   
+      if (!updatedFighter.id) {
+        res.err = 'Fighter not found'
+        res.errCode = 404
+      } else {
+        res.data = updatedFighter
+      }
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
+    }
+  },
+  responseMiddleware,
+)
+
+//delete fighter
+router.delete(
+  '/:id',
+  (req, res, next) => {
+    try {
+      const { id } = req.params
+      const deletedFighter = fighterService.delete(id)
+
+      if (!deletedFighter.length) {
+        res.err = 'Fighter not found'
+        res.errCode = 404
+      } else {
+        res.data = deletedFighter
+      }
+    } catch (e) {
+      res.err = e
+      res.errCode = 500
+    } finally {
+      next()
+    }
+  },
+  responseMiddleware,
+)
 
 export { router };
