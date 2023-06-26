12936256b3152be7d427a3e987b7451946cd7cd9
AndriySt92 <andriyua-1992@mail.ru>
2023-06-26 11:07:54 +0300

    Cleaned up extra import in fight.validation.middleware.js

--- middlewares/fight.validation.middleware.js
+++ middlewares/fight.validation.middleware.js
@@ -1,17 +1,11 @@
 import { FIGHT } from '../models/fight.js'
-import {
-  checkIsEmptyObj,
-  checkFieldLength,
-  checkIsInvalidFields,
-  validateNumber,
-  checkIsUniqueName,
-} from '../validation/index.js'
+import { checkIsEmptyObj, checkIsInvalidFields } from '../validation/index.js'
 import { fighterService } from '../services/fighterService.js'
 
 const createFightValid = (req, res, next) => {
   const fightData = req.body
   const { id, fighter1, fighter2, log } = fightData
-    
+
   try {
     if (checkIsEmptyObj(fightData)) {
       throw new Error('Payload should not be empty!')
@@ -45,9 +39,9 @@ const updateFightValid = (req, res, next) => {
       throw new Error('Payload contain invalid fields')
     } else if (id) {
       throw new Error('The id field should not be present!')
-    } else if (fighter1 && !fighterService.search({ id:fighter1 })) {
+    } else if (fighter1 && !fighterService.search({ id: fighter1 })) {
       throw new Error('Invalid payload. Fighter1 not found')
-    } else if (fighter2 && !fighterService.search({ id:fighter2 })) {
+    } else if (fighter2 && !fighterService.search({ id: fighter2 })) {
       throw new Error('Invalid payload. Fighter2 not found')
     } else if (log && Array.isArray(log) && log.lenght > 0) {
       throw new Error('Invalid log field')
